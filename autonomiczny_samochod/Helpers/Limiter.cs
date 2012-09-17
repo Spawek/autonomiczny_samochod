5df39d347fbb25e590feddd6a1e9bb41661aa763
Spawek <spawnerer@gmail.com>
2012-09-17 22:18:19 +0200

    Added files, which should be added long time ago (it wouldn't even compile without them)

---
+++ autonomiczny_samochod/Helpers/Limiter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace autonomiczny_samochod
+{
+    class Limiter
+    {
+        /// <summary>
+        /// makes sure that variable is in range of [lowerLimit, upperLimit]
+        /// </summary>
+        /// <param name="var"></param>
+        /// <param name="lowerLimit"></param>
+        /// <param name="upperLimit"></param>
+        /// <returns>returns value</returns>
+        public static double Limit(ref double var, double lowerLimit, double upperLimit)
+        {
+            if (var < lowerLimit)
+            {
+                var = lowerLimit;
+            }
+            else if (var > upperLimit)
+            {
+                var = upperLimit;
+            }
+
+            return var;
+        }
+    }
+}
