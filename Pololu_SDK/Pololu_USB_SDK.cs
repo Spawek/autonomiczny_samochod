3c08c2bf5abfd53e565ad7f4deac01c74015e603
Spawek <spawnerer@gmail.com>
2012-09-22 17:01:39 +0200

    added POLOLU_USB_SDK as subproject

---
+++ Pololu_SDK/Pololu_USB_SDK.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+using Pololu.Usc;
+using Pololu.UsbWrapper;
+
+namespace Pololu_SDK
+{
+    public class Pololu_USB_SDK
+    {
+        /// <summary>
+        /// Attempts to set the target (width of pulses sent) of a channel.
+        /// </summary>
+        /// <param name="channel">Channel number from 0 to 23.</param>
+        /// <param name="target">
+        ///   Target, in units of quarter microseconds.  For typical servos,
+        ///   6000 is neutral and the acceptable range is 4000-8000.
+        /// </param>
+        public void TrySetTarget(Byte channel, UInt16 target)
+        {
+            try
+            {
+                using (Usc device = connectToDevice())  // Find a device and temporarily connect.
+                {
+                    device.setTarget(channel, target);
+
+                    // device.Dispose() is called automatically when the "using" block ends,
+                    // allowing other functions and processes to use the device.
+                }
+            }
+            catch (Exception exception)  // Handle exceptions by displaying them to the user.
+            {
+                displayException(exception);
+            }
+        }
+
+        /// <summary>
+        /// Connects to a Maestro using native USB and returns the Usc object
+        /// representing that connection.  When you are done with the
+        /// connection, you should close it using the Dispose() method so that
+        /// other processes or functions can connect to the device later.  The
+        /// "using" statement can do this automatically for you.
+        /// </summary>
+        public Usc connectToDevice()
+        {
+            // Get a list of all connected devices of this type.
+            List<DeviceListItem> connectedDevices = Usc.getConnectedDevices();
+
+            foreach (DeviceListItem dli in connectedDevices)
+            {
+                // If you have multiple devices connected and want to select a particular
+                // device by serial number, you could simply add a line like this:
+                //   if (dli.serialNumber != "00012345"){ continue; }
+
+                Usc device = new Usc(dli); // Connect to the device.
+                return device;             // Return the device.
+            }
+            throw new Exception("Could not find device.  Make sure it is plugged in to USB " +
+                "and check your Device Manager (Windows) or run lsusb (Linux).");
+        }
+
+        /// <summary>
+        /// Displays an exception to the user by popping up a message box.
+        /// </summary>
+        private void displayException(Exception exception)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            do
+            {
+                stringBuilder.Append(exception.Message + "  ");
+                if (exception is Win32Exception)
+                {
+                    stringBuilder.Append("Error code 0x" + ((Win32Exception)exception).NativeErrorCode.ToString("x") + ".  ");
+                }
+                exception = exception.InnerException;
+            }
+            while (exception != null);
+            MessageBox.Show(stringBuilder.ToString(), "DUNNO WHAT TEXT IS THIS - CHANGE IT!:", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+
+}
