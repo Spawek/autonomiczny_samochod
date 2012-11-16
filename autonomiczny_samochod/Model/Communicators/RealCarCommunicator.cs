54a6944eaefbc9589302142cde660f6c40f233a1
Spawek <spawnerer@gmail.com>
2012-11-16 14:46:27 +0100

    removing Pololu_SDK sub-project - leaving just needed .dll files removing car_communicator sub-project - moving files from in to autonomiczny_samochod project

--- autonomiczny_samochod/Model/Communicators/RealCarCommunicator.cs
+++ autonomiczny_samochod/Model/Communicators/RealCarCommunicator.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using autonomiczny_samochod.Model.Communicators;
 using car_communicator;
 
 namespace autonomiczny_samochod
