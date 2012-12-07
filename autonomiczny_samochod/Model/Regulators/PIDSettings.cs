ef33b112a726b97a0e80c87b887c27b4c397abd3
Spawek <spawnerer@gmail.com>
2012-12-07 19:01:41 +0100

    moving PIDRegulator.cs to Helpers adding forgotten Linux/UsbDevice.cs file

--- autonomiczny_samochod/Model/Regulators/PIDSettings.cs
+++ autonomiczny_samochod/Model/Regulators/PIDSettings.cs
@@ -8,22 +8,22 @@ namespace autonomiczny_samochod.Model.Regulators
     abstract class PIDSettings
     {
         //P part settings
-        public double P_FACTOR_MULTIPLER; // = 80.0;
+        public double P_FACTOR_MULTIPLER;
 
         //I part settings
-        public double I_FACTOR_MULTIPLER; // = 10.0; //2.0; //0.4; //hypys radzi, żeby to wyłączyć bo może być niestabilny (a tego baardzo nie chcemy)
-        public double I_FACTOR_SUM_MAX_VALUE; // = 250.0;
-        public double I_FACTOR_SUM_MIN_VALUE; // = -250.0;
-        public double I_FACTOR_SUM_SUPPRESSION_PER_SEC; // = 0.88; //1.0 = suppresing disabled
+        public double I_FACTOR_MULTIPLER;
+        public double I_FACTOR_SUM_MAX_VALUE; // 100 is normal value
+        public double I_FACTOR_SUM_MIN_VALUE; // -100 is normal value
+        public double I_FACTOR_SUM_SUPPRESSION_PER_SEC; //1.0 = suppresing disabled
 
         //D part settings
-        public double D_FACTOR_MULTIPLER; // = 120.0;
-        public double D_FACTOR_SUPPRESSION_PER_SEC; // = 0.78;
-        public double D_FACTOR_SUM_MIN_VALUE;
-        public double D_FACTOR_SUM_MAX_VALUE;
+        public double D_FACTOR_MULTIPLER; 
+        public double D_FACTOR_SUPPRESSION_PER_SEC; //1.0 = suppressing disabled //
+        public double D_FACTOR_SUM_MIN_VALUE; // 100 is normal value
+        public double D_FACTOR_SUM_MAX_VALUE; // -100 is normal value
 
         //steering limits
-        public double MAX_FACTOR_CONST; // = 100.0;
-        public double MIN_FACTOR_CONST; // = -100.0;
+        public double MAX_FACTOR_CONST; // 100 is normal value
+        public double MIN_FACTOR_CONST; // -100.0 is normal value
     }
 }
